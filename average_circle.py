549db0d0962f98a568d4bdd8832c721771431895
ehavazli <emre.havazli@gmail.com>
2017-08-17 17:29:58 -0400

    automatically increase distance and write to csv

--- average_circle.py
+++ average_circle.py
@@ -7,6 +7,7 @@ import h5py
 import pysar
 import pysar._readfile as readfile
 import pysar._pysar_utilities as ut
+import csv
 
 def main(argv):
     try:
@@ -14,11 +15,13 @@ def main(argv):
         y = int(argv[1])
         x = int(argv[2])
         radius = int(argv[3])
+        snum_pix = int(argv[4])
+        enum_pix = int(argv[5])
     except:
         print '''
     *******************************************
 
-       Usage: average_circle.py h5file y x radius
+       Usage: average_circle.py h5file y x start_num_pix end_num_pix
 
     *******************************************
     '''
@@ -30,17 +33,28 @@ def main(argv):
     ref_vel = (velocity[y][x])*1000.0 #convert velocity values from meters to mm
     print 'REF VEL: ' + str(ref_vel)
     atr = readfile.read_attribute(velocityfile)
-    crc_mask = ut.circle_index(atr,(y,x,radius))
-    masked_vel = (velocity[crc_mask])*1000.0 #convert velocity values from meters to mm
-    print 'MASKED VEL: ' +str(masked_vel)
-    diff_masked_vel = (ref_vel)-(masked_vel)
-    print 'DIFF MASKED VEL: '+str(diff_masked_vel)
-    average_crc = mean(diff_masked_vel)
-    std_masked = std(diff_masked_vel,dtype=float64)
-#    print 'Masked Vel: ' + str(masked_vel)
-#    print 'Referenced Vel: ' +str(diff_masked_vel)
-    print 'Average of referenced vel in the circle: ' +str(average_crc)+' mm/yr'
-    print 'Standard Dev of referenced vel in the Circle: ' +str(std_masked)
+
+    csv_file = open('summary_mean_std.csv',"wb")
+    csv_file = open('summary_mean_std.csv',"wb")
+    writer = csv.writer(csv_file)
+    writer.writerow(('Distance (km)', 'Time Series Length (years)','Average (mm/yr)', 'Standard Deviation (mm/yr)'))
+
+    for i in range(snum_pix,(enum_pix+1)):
+        dist= (i/10.0) #convert distance to km
+        ts_year = velocityfile.split('_')[2]
+        crc_mask = ut.circle_index(atr,(y,x,i))
+        masked_vel = (velocity[crc_mask])*1000.0 #convert velocity values from meters to mm
+    #    print 'MASKED VEL: ' +str(masked_vel)
+        diff_masked_vel = (ref_vel)-(masked_vel)
+    #    print 'DIFF MASKED VEL: '+str(diff_masked_vel)
+        average_crc = mean(diff_masked_vel,dtype=float64)
+        std_masked = std(diff_masked_vel,dtype=float64)
+        writer.writerow(dist,ts_year,average_crc,std_masked)
+    #    print 'Masked Vel: ' + str(masked_vel)
+    #    print 'Referenced Vel: ' +str(diff_masked_vel)
+    #    print 'Average of referenced vel in the circle: ' +str(average_crc)+' mm/yr'
+    #    print 'Standard Dev of referenced vel in the Circle: ' +str(std_masked)
+    csv_file.close()
 ###########################
 if __name__ == '__main__':
     main(sys.argv[1:])
