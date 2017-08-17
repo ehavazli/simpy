81aa3d1237a5c8ebd622157c0669b94e50467f05
ehavazli <emre.havazli@gmail.com>
2017-08-17 13:10:52 -0400

    removed obsolete lines

--- auto_pilot_trop.py
+++ auto_pilot_trop.py
@@ -31,17 +31,11 @@ def main(argv):
     *******************************************
     '''
         sys.exit(1)
-    
+
     for i in xrange(start_yr,end_yr+1):
         n_im = int(math.ceil((365.0/35.0)*i))
         for n in xrange(0,noft):
             x = os.path.basename('TS'+str(i)+'-'+str(n))
-#            gname = x+'.template'
-#            g = open(gname,'w')
-#            g.write('pysar.inputFiles     = /nethome/ehavazli/SIM/synth_data_'+str(i)+str(n)+'/*.unw')
-#            g.write('\npysar.corFiles       = /nethome/ehavazli/SIM/synth_data_'+str(i)+str(n)+'/*.cor')
-#            g.write('\npysar.seed.yx     = 100 , 100')
-#            g.close()
 
             fname=x+'.process'
             f = open(fname,'w')
@@ -51,10 +45,8 @@ def main(argv):
             f.write('\n#BSUB -o '+x+'.o%J')
             f.write('\n#BSUB -e '+x+'.e%J')
             f.write('\n#BSUB -W 24:00')
-#            f.write('\n#BSUB -W '+Walltime)
             f.write('\n#BSUB -q general')
             f.write('\n#BSUB -n 1')
-  #          f.write('\n#BSUB -R "span[hosts=1]"')
             f.write('\n#BSUB -B')
             f.write('\n#BSUB -N')
 #            f.write('\n#BSUB -u e.havazli@rsmas.miami.edu')
@@ -64,16 +56,6 @@ def main(argv):
             f.write('\ncd ./syn'+str(i)+'-'+str(n))
             f.write('\nload_ref2ts.py .')
             f.write('\ntimeseries2velocity.py timeseries_sim.h5 -o velocity_sim.h5')
-#            f.write('\nsimulate.py '+str(n_im)+' '+str(n_days)+' ./syn'+str(i)+str(n)+'/ '+ signal)
-#            f.write('\ninterfero.py '+str(i)+str(n))
-#            f.write('\nload_data_sim.py ~/SIM/TS'+str(i)+'-'+str(n)+'.template')
-#            f.write('\nmv Pairs_'+str(i)+str(n)+'.png $TSSARDIR/TS'+str(i)+'-'+str(n))
-#            f.write('\nmv syn'+str(i)+str(n)+' $TSSARDIR/TS'+str(i)+'-'+str(n))
-#            f.write('\nmv synth_data'+str(i)+str(n)+' $TSSARDIR/TS'+str(i)+'-'+str(n))
-#            f.write('\npysarApp.py ~/SIM/TS'+str(i)+'-'+str(n)+'.template '+ '--dir $TSSARDIR/TS'+str(i)+'-'+str(n))
-#            f.write('\nmv Pairs_'+str(i)+str(n)+'.png $TSSARDIR/TS'+str(i)+'-'+str(n))
-#            f.write('\nmv syn'+str(i)+str(n)+' $TSSARDIR/TS'+str(i)+'-'+str(n))
-#            f.write('\ntimeseries2velocity.py -f $TSSARDIR/TS'+str(i)+'-'+str(n)+'/timeseries.h5 -o $TSSARDIR/TS'+str(i)+'-'+str(n)+'/velocity.h5')
 
             f.close()
             jobCmd='bsub -P sinkhole < '+fname
