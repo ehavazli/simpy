1b1bd53c7b02d14d7fa235a3c146bc4f05ca5bb5
ehavazli <emre.havazli@gmail.com>
2017-08-20 18:13:19 -0400

    fixed bug

--- average_circle.py
+++ average_circle.py
@@ -36,7 +36,7 @@ def main(argv):
         #    print 'REF VEL: ' + str(ref_vel)
             atr = readfile.read_attribute(velocityfile)
             ts_year = velocityfile.split('_')[2]
-            csv_file = open('summary_'+str(ts_year)+'years_mean_std_'+str(y)+'.csv',"wb")
+            csv_file = open('summary_'+str(ts_year)+'years_mean_std_'+str(k)+'.csv',"wb")
             writer = csv.writer(csv_file)
             writer.writerow(('Distance (km)', 'Time Series Length (years)','Average (mm/yr)', 'Standard Deviation (mm/yr)'))
 
