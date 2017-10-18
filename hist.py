548f95e42af7b5cfcc42f3be59576bf7b4167839
ehavazli <emre.havazli@gmail.com>
2017-10-18 13:41:23 -0400

    plot historgrams

---
+++ hist.py
@@ -0,0 +1,56 @@
+#! /usr/bin/env python
+#@author: Emre Havazli
+
+import os
+import sys
+from numpy import *
+import matplotlib
+import glob
+import h5py
+matplotlib.use("TkAgg")
+import matplotlib.pyplot as plt
+from osgeo import gdal
+
+
+def main(argv):
+    try:
+        directory = argv[1]
+    except:
+        print '''
+    *******************************************
+
+       Usage: hist.py [directory]
+
+              directory : location of the TS folders (where syn folders are)
+
+    *******************************************
+    '''
+        sys.exit(1)
+
+
+    ts_list = glob.glob(directory+'/syn*')
+    velocities = {}
+    for n in ts_list:
+        spl = n.split('syn')
+        pick_yr = spl[-1]
+        year,test = pick_yr.split('-')
+        year = int(year)
+        if year in velocities: pass
+        else:
+            print 'Working on '+str(year)+' year long time series'
+            spl_lst = glob.glob(directory+'/syn'+str(year)+'*')
+            for i in spl_lst:
+                velocity_file = i +'/velocity_sim.h5'
+                f = h5py.File(velocity_file,'r')
+                dset = f['velocity'].get('velocity')
+
+                dset_hist = (asarray(dset)*1000.0)
+                bin_values = arange(start=-5, stop=5, step=0.001)
+                plt.hist(dset_hist.flatten(),bins=bin_values,normed=1,color='blue',histtype='stepfilled')
+                plt.ylabel('PDF',fontsize=14)
+                plt.xlabel('Velocity (mm/yr)',fontsize=14)
+                plt.savefig(i+'/hist_'+str(i[-4:])+'.tiff', bbox_inches='tight', dpi = 300)
+                plt.close()
+
+if __name__ == '__main__':
+    main(sys.argv[:])
