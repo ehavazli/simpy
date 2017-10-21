38abd56d0278c4974ae746cd8dd0497197577586
ehavazli <emre.havazli@gmail.com>
2017-10-21 19:33:23 -0400

    bug fix for combined histogram

--- hist.py
+++ hist.py
@@ -52,6 +52,11 @@ def main(argv):
                 plt.xlabel('Velocity (mm/yr)',fontsize=14)
                 plt.savefig(i+'/hist_'+str(i[-4:])+'.tiff', bbox_inches='tight', dpi = 300)
                 plt.close()
-            velocities[year] = asarray(data_to_plot)    
+            velocities[year] = asarray(data_to_plot)
+            plt.hist(data_to_plot.flatten(),bins=bin_values,normed=1,color='blue',histtype='stepfilled')
+            plt.ylabel('PDF',fontsize=14)
+            plt.xlabel('Velocity (mm/yr)',fontsize=14)
+            plt.savefig('./hist_'+str(year)+'_combined.tiff', bbox_inches='tight', dpi = 300)
+            plt.close()
 if __name__ == '__main__':
     main(sys.argv[:])
