c50eb07fc91fd9610f41f95b31d7896282eec3e8
ehavazli <emre.havazli@gmail.com>
2017-10-21 20:35:08 -0400

    bug fix

--- hist.py
+++ hist.py
@@ -52,7 +52,7 @@ def main(argv):
                 plt.xlabel('Velocity (mm/yr)',fontsize=14)
                 plt.savefig(i+'/hist_'+str(i[-4:])+'.tiff', bbox_inches='tight', dpi = 300)
                 plt.close()
-            velocities[year] = asarray(data_to_plot)
+            velocities[year] = (asarray(data_to_plot)*1000.0)
             plt.hist(velocities[year].flatten(),bins=bin_values,normed=1,color='blue',histtype='stepfilled')
             plt.ylabel('PDF',fontsize=14)
             plt.xlabel('Velocity (mm/yr)',fontsize=14)
